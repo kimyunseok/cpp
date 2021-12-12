0db4690168cab4db0690477c287c743cc1057e99
KHS-PC\pskna <psknal27@gmail.com>
2021-12-13 01:07:52 +0900

    OOP - 얕은 복사와 깊은 복사
    - 얕은 복사 : 복사 생성자를 이용해서 객체를 복사할 때 원본 객체를 그대로 복사해서 생성한다.
    원본 객체 내부에 주소 참조 변수(포인터)가 있을 경우에 복사 객체가 해당 주소를 그대로 참조하게 된다.
    
    - 깊은 복사 : 복사 생성자를 이용해서 객체를 복사할 때, 원본 객체 내부에 주소 참조 변수(포인터)가 있을 경우에
    해당 주소가 가리키는 변수의 값을 역참조해서 그 값을 나타내는 변수를 Heap 메모리에 새로 동적할당해서
    새로 동적할당한 주소를 가리키도록 만드는 방법이다.

---
+++ study/shallow_copy_and_deep_copy.cpp
@@ -0,0 +1,61 @@
+﻿/*
+* OOP - 얕은 복사와 깊은 복사
+*/
+#include <iostream>
+using namespace std;
+
+class MyObject {
+public:
+    string* name;
+    int age;
+
+    //기본 생성자
+    MyObject(int age, string name) {
+        this->name = new string(name);
+        this->age = age;
+    }
+
+    //기본 복사 생성자와 같음. 레퍼런스로 만들지 않으면 복사생
+    //MyObject(const MyObject &myObj) {
+    //    cout << "얕은복사 생성자 호출" << "\n";
+    //    this->name = myObj.name;
+    //    this->age = myObj.age;
+    //}
+
+    //깊은 복사 생성자.
+    MyObject(const MyObject &myObj) {
+        cout << "깊은복사 생성자 호출" << "\n";
+        this->name = new string(*myObj.name);
+        this->age = myObj.age;
+    }
+
+    //소멸자
+    ~MyObject() {
+        cout << *name << "의 소멸자 호출" << "\n";
+        delete name;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    MyObject obj1(42, "Mike");
+    MyObject obj2(obj1);
+    //MyObject obj2 = obj1;
+
+    *obj2.name = "John";
+    obj2.age = 36;
+
+    cout << "obj1's name's addr:" << &(*obj1.name) << "\n";
+    cout << "obj2's name's addr:" << &(*obj2.name) << "\n";
+
+    cout << "obj1's name:" << *obj1.name << "\n";
+    cout << "obj2's name:" << *obj2.name << "\n";
+
+    cout << "obj1's age:" << obj1.age << "\n";
+    cout << "obj2's age:" << obj2.age << "\n";
+
+    return 0;
+}
